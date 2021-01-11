18fc59acc8de5d8cf0aa53e408ddce7718d1817a
rnemet <robert.nemet@ricardo.ch>
2021-01-11 20:22:48 +0100

    small chamges, makefile

---
+++ scripts/rf.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+# Script name: rf.sh
+# 
+# Author: robert.nemet@gmail.com
+#
+# Description: Rename files that matches a PATTERN from behind to match pattern with SUFIX and POSTFIX, while PATTERN is removed.
+# 
+# Example: Files mathing *file.xyz are renamed to document-1.txt
+
+PATTERN=$1
+SUFIX=$2
+POSTFIX=$3
+
+declare -i cnt=0
+for f in $(eval echo "*$PATTERN"); do
+    mv "$f" "$SUFIX${f%$PATTERN}$POSTFIX"
+    cnt=$cnt+1
+done;
+
+echo "renamed $cnt files."
